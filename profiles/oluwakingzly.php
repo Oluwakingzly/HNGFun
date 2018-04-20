e01ca48c61d941ff6a9b55063b3979798e6bc00b
okelarin kingsley <hardexyz@gmail.com>
2018-04-20 14:26:52 +0100

    done

---
+++ profiles/oluwakingzly.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>chefvys</title>
+		<style type="text/css">
+			 header, section, footer, aside, nav, article, figure, figcaption {
+			 	display: block;
+			 } 
+			 body{
+			 	color: #666666;     
+			 	background-color: #f9f8f6;               
+			 	font-family: Georgia, times, serif;     
+			 	line-height: 1.4em;     
+			 	margin: 0px;
+			 }
+			 .wrapper {     
+			 	width: 940px;     
+			 	margin: 20px auto 20px auto;     
+			 	border: 2px solid #000000;     
+			 	background-color: #ffffff;
+			 } 
+		
+			 }
+			 h1 {     
+			 	text-indent: -9999px;     
+			 	width: 940px;     
+			 	height: 130px;     
+			 	margin: 0px;
+			 }
+			  nav, footer {     
+			  	clear: both;     
+			  	color: #ffffff;     
+			  	background-color: #aeaca8;     
+			  	height: 30px;
+			 } 
+			   nav ul {     
+			   	margin: 0px;     
+			   	padding: 5px 0px 5px 30px;
+			 } 
+			 nav li {     
+			 	display: inline;     
+			 	margin-right: 40px;
+			 }
+			 nav li a {
+			 	color: #ffffff;
+			 }
+			 nav li a:hover, nav li a.current {     
+			 	color: #000000;
+			 }    
+			 section.courses {     
+			 	float: left;     
+			 	width: 659px;     
+			 	border-right: 1px solid #eeeeee;
+			 }    
+			 article {     
+			 	clear: both;     
+			 	overflow: auto;     
+			 	width: 100%;
+			 }    
+			 hgroup {     
+			 	margin-top:40px;
+			 } 
+			 figure {     
+			 	float: left;     
+			 	width: 290px;     
+			 	height: 220px;     
+			 	padding: 5px;     
+			 	margin: 20px;     
+			 	border: 1px solid #eeeeee;
+			
+			 }    
+			 aside {     
+			 	width: 230px;     
+			 	float: left;     
+			 	padding: 0px 0px 0px 20px;
+			 }    
+			 aside section a {    
+			  display: block;     
+			  padding: 10px;     
+			  border-bottom: 1px solid #eeeeee;
+			  }   
+			 aside section a:hover {     
+			 	color: #985d6a;     
+			 	background-color: #efefef;
+			 }
+			  a {     
+			  	color: #de6581;     
+			  	text-decoration: none;
+			 }    
+			  h1, h2, h3 {     
+			  	font-weight: normal;
+			  	}    
+			  h2 {  
+			  	margin: 10px 0px 5px 0px;     
+			  	padding: 0px;
+			  	}    
+			  	h3 {    
+			  	 margin: 0px 0px 10px 0px;     
+			  	 color: #de6581;
+			  	 }    
+			  	 aside h2 {    
+			  	  padding: 30px 0px 10px 0px;     
+			  	  color: #de6581;
+			  	}    
+			  	  footer {     
+			  	  	font-size: 80%;     
+			  	  	padding: 7px 0px 0px 20px;
+			  	  	text-align: center;
+			  	  }
+			  	  h1.king{text-align: center;
+
+			  	  } 
+			  	  
+
+		</style>
+	</head>
+	<body>
+		<div class="wrapper">
+			<header>
+				<h1 class="king">Okelarin Kingsley A.</h1>
+				<nav>
+					OKELARIN'S PROFILE
+				</nav>
+			</header>
+			<section  class="courses">
+				<article>
+
+				</article>
+					<hgroup>
+						<h2>okelarin Kingsley</h2>
+						<h3>Nigerian</h3>
+						<h3>Lagosian</h3>
+						<h3>Lover of Technology</h3>
+						<h3>Cryptocurrency $ Airdrops</h3>
+					</hgroup>
+					<p>hotels.ng have given me another chance to develop myself and am taking advantage of it. 
+					i love technology and it amuses mw how to type something with your keyboard and viola, its done
+					. dont you just love technology. <br>
+					on the flip side, i invest my time to cryptocurrency, lol bitcoin, ethereum, litecoin name it, all covered</p> 
+				</article>
+			</section>
+				<aside>
+					<section class="popular-recipes">
+						<h2>Hobby</h2>
+						<p>listening to music</p>
+						<p>Dancing</p>
+						<p>Making friends</p>
+						<p>coding</p>
+					</section>
+					<section class="contact-detail">
+						<h2>contact</h2>
+						<a href="twitter.com/oluwakingzly">Twitter</a>
+						<a href="facebook.com"/oluwakingzly>Facebook</a>
+					</section>
+				</aside>
+				<footer>
+					&copy;: K'larin Oluwakingzly &trade;
+				</footer>
+		</div> <!--wrapper-->	
+	</body>
+</html>
